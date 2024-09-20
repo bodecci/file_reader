2c898192a137e204d8eb49f1b68489e318144808
Bode <bode.falade@outlook.com>
2024-09-20 14:30:15 -0500

    refactor to pass file path.

--- src/main.go
+++ src/main.go
@@ -8,10 +8,20 @@ import (
 )
 
 func main() {
-	file, err := os.Open("data.txt")
-	if err != nil {
-		log.Fatal(err)
+
+	// Check if a file path was provided as a command-line argument
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a file path")
 	}
+
+	// Open the file specified as the first command-line argument
+	filePath := os.Args[1]
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("Failed to open the file: %v", err)
+	}
+	defer file.Close() // Ensure the file is closed after the function returns
+
 	// Create a new scanner to read the file
 	scanner := bufio.NewScanner(file)
 
@@ -19,13 +29,9 @@ func main() {
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 	}
-	err = file.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	// Check for errors that occurred during the scan
 	if scanner.Err() != nil {
-		log.Fatal(err)
+		log.Fatalf("Error during scanning: %v", err)
 	}
 }
